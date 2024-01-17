df3616dd645529f4a60235d5289e6177909ac489
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2024-01-16 21:12:28 -0400

    add whiteboard file

---
+++ Python-Scientific-Computing/file0_whiteboard.py
@@ -0,0 +1,58 @@
+x = 1
+print(x)
+
+x = x + 1
+print(x)
+
+x = x + 2
+print(x)
+
+# Conditional Steps___________________________________________________
+
+x = 5
+
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+    
+print('Finish')
+
+# Loops_______________________________________________________________
+
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print('Blastoff!')
+
+#5
+#4
+#3
+#2
+#1
+#Blastoff!
+
+# Chapter II: Variables, Expressions, and Statements___________________
+
+hours = 35.0
+rate = 12.5
+pay = hours * rate
+print(pay)  # log: 437.5
+
+xx = 2
+xx = xx + 2
+print(xx) # log:4
+
+yy = 440 * 12
+print(yy) # log: 5280
+
+zz = yy / 1000
+print(zz) # log: 5.28
+
+jj = 23
+kk = jj % 5
+print(kk) # log: 3
+
+
+
